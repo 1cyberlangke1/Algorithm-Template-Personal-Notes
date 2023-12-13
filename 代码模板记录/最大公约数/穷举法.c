bf575bac6d0911adae2106755350a2a8817e84f6
1cyberlangke1 <145818885+1cyberlangke1@users.noreply.github.com>
2023-12-13 15:02:11 +0800

    Update 穷举法.c

--- 代码模板记录/最大公约数/穷举法.c
+++ 代码模板记录/最大公约数/穷举法.c
@@ -6,7 +6,8 @@ int exhgcd(int a,int b) {
 		a = b;
 		b = tmp;
 	}//a>b
-	if (a == 0 || b == 0)return 0;//非法数据检查
+	if (a == 0)return b;
+	if (b == 0)return a;
 	for (int i = b; i >= 1; i--) {
 		if (a % i == 0 && b % i == 0)return i;
 	}
