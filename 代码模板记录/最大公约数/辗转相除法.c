5ffe5e8a540f1e3723d5ae3d703dfc31e206287a
1cyberlangke1 <145818885+1cyberlangke1@users.noreply.github.com>
2023-12-13 15:01:53 +0800

    Update 辗转相除法.c

--- 代码模板记录/最大公约数/辗转相除法.c
+++ 代码模板记录/最大公约数/辗转相除法.c
@@ -6,7 +6,8 @@ int eucgcd(int a,int b) {
 		a = b;
 		b = tmp;
 	}//a>b
-	if (a == 0 || b == 0)return 0;//非法数据检查
+	if (a == 0)return b;
+	if (b == 0)return a;
 	if (a % b == 0)return b;
 	else return eucgcd(a, a % b);
 }
