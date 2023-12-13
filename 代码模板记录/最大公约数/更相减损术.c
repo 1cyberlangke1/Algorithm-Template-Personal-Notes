291085b54a59c732f3b2e5fd94875d7d2866b68b
1cyberlangke1 <145818885+1cyberlangke1@users.noreply.github.com>
2023-12-13 15:13:17 +0800

    Update 更相减损术.c

--- 代码模板记录/最大公约数/更相减损术.c
+++ 代码模板记录/最大公约数/更相减损术.c
@@ -1,14 +1,22 @@
 #include<stdio.h>
 
 int subgcd(int a, int b) {
-	if (a == 0 || b == 0)return 0;//非法数据检查
+	if (a == 0)return b;
+	else if (b == 0)return a;
 	else if (a == b)return a;
-	if (a > b)return subgcd(a - b, b);
-	else return (a, b - a);
+	if (a < b) {
+		int tmp = a;
+		a = b;
+		b = tmp;
+	}//a>b
+	if (a % 2 == 0 && b % 2 == 0)return subgcd(a >> 1, b >> 1) << 1;
+	else if (a % 2 == 0)return subgcd(a >> 1, b);
+	else if (b % 2 == 0)return subgcd(a, b >> 1);
+	subgcd(a - b, b);
 }
 
 int main() {
 	//使用例
 	int a = 72, b = 108;
-	printf("%d", subgcd(a,b));
+	printf("%d", subgcd(a, b));
 }
