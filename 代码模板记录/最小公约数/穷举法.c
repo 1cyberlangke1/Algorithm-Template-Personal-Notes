be463d42317d2802b8f80587ce59d02175d5117f
1cyberlangke1 <145818885+1cyberlangke1@users.noreply.github.com>
2023-12-13 14:08:56 +0800

    Create 穷举法.c

---
+++ 代码模板记录/最小公约数/穷举法.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int exhgcd(int a,int b) {
+	if (a < b) {
+		int tmp = a;
+		a = b;
+		b = tmp;
+	}//a>b
+	if (a == 0 || b == 0)return -1;//非法数据检查
+	for (int i = b; i >= 1; i--) {
+		if (a % i == 0 && b % i == 0)return i;
+	}
+}
+
+int main() {
+	//使用例
+	int a = 20， b = 15;
+	printf("%d"， exhgcd(a,b));
+}
