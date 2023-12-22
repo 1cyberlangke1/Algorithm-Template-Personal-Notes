af9fe6866f3180a896f230e9b623cd031c5d6958
1cyberlangke1 <145818885+1cyberlangke1@users.noreply.github.com>
2023-12-22 21:57:29 +0800

    Update 辗转相除法.c

--- 代码模板记录/最大公约数/辗转相除法.c
+++ 代码模板记录/最大公约数/辗转相除法.c
@@ -1,20 +1,11 @@
 #include<stdio.h>
 
-int eucgcd(int a,int b) {
-	if (a < b) {
-		int tmp = a;
-		a = b;
-		b = tmp;
-	}//a>b
-	if (a == 0)return b;
-	else if (b == 0)return a;
-	else if(a == b)return a;
-	else if (a % b == 0)return b;
-	else return eucgcd(a, a % b);
+int eucgcd(int a, int b) {
+	return b ? eucgcd(b, a % b) : a;
 }
 
 int main() {
 	//使用例
 	int a = 72, b = 108;
-	printf("%d", eucgcd(a,b));
+	printf("%d", eucgcd(a, b));
 }
